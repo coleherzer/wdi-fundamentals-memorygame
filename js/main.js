e6b871f41d0b5ed0d101516568a2568d4b477490
coleherzer <cole.herzer@gmail.com>
2017-08-20 14:35:51 -0700

    updated js for unit 10 assignment

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 
 var cardsInPlay = [];
 
@@ -11,11 +32,13 @@ var checkForMatch = function () {
 }
 
 var flipCard = function (cardId) {
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 	
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 
-	if (cardsInPlay.length === 2); {
+	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
